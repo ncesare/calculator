fa0130fff345d0a09e883f5f9f8b312276f30fcd
Nick Cesare <ncesare@gmail.com>
2022-09-24 15:11:24 -0600

    Initial operations sketch

--- script.js
+++ script.js
@@ -1,111 +1,58 @@
-const display = document.querySelector('#display-box');
-
-let expression = [];
+const displayBox = document.querySelector('#display-box')
 
 let clearSlate = true;
 
-const allClear = document.querySelector('#all-clear');
-allClear.addEventListener('click', () => {
-    display.textContent = '0';
-    expression = [];
-    });
+let expression = [];
 
-const negativeOperator = document.querySelector('#negative-op');
-const percentOperator = document.querySelector('#percent-op');
-const divOperator = document.querySelector('#div-op');
-const multOperator = document.querySelector('#mult-op');
-const plusOperator = document.querySelector('#plus-op');
-const minusOperator = document.querySelector('#minus-op');
-const equalsOperator = document.querySelector('#equals-op');
+const buttons = document.querySelectorAll('button');
 
-negativeOperator.addEventListener('click', () => {
-    if (display.textContent.includes('-')) {
-        display.textContent = Math.abs(Number(display.textContent));
-    } else {
-        display.textContent = '-' + display.textContent;
-    }})
+buttons.forEach(button => button.addEventListener('click', () => {
+    if (!isNaN(button.value)) appendNumber(button.value);
+    else operationSwitch(button.value);
+}))
 
-percentOperator.addEventListener('click', () => display.textContent = parseFloat(display.textContent)/100)
-
-const operators = [divOperator, multOperator, plusOperator, minusOperator]
-
-for (operator of operators) {
-    operator.addEventListener('click', (e) => expression = operation(e));
+function appendNumber(digit) { //Appends a number to the display
+    if (clearSlate) {
+        displayBox.textContent = digit;
+        clearSlate = false;
+    } else displayBox.textContent += digit;
 }
 
-equalsOperator.addEventListener('click', () => expression = calculate(expression));
-
-const numbers = document.querySelectorAll('.number');
-
-numbers.forEach(number => {
-    number.addEventListener('click', (e) => {
-        // Catch errors that would cause invalid or too-long numbers to appear
-        if (e.target.value === '.' && display.textContent.includes('.'))
-            return;
-
-        if (display.textContent.length >= 11) {
-            console.log('Display full');
-            return;
-        }
-
-        if (display.textContent != 0 && expression.length == 0) {
-            clearSlate = false;
-        }
-
-        if (clearSlate) {
-            display.textContent = e.target.value;
-        } else {
-            display.textContent = display.textContent + e.target.value;
-        }
-    });
-})
-
-// Bind the user's entered number and chosen operation
-
-function operation(e) {
-    if (expression.length > 0) {
-        calculate(expression);
-        clearSlate = true;
+function operationSwitch(value) {
+    switch(value) {
+        case 'clear':
+            displayBox.textContent = 0;
+            clearSlate = true;
+            expression = [];
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            expression.push(Number(displayBox.textContent), value);
+            console.log(expression);
+            clearSlate = true;
+            break;
+        case 'negative':
+            if (displayBox.textContent.includes('-')) displayBox.textContent = Math.abs(displayBox.textContent);
+            else displayBox.textContent = '-' + displayBox.textContent;
+            break;
+        case '%':
+            displayBox.textContent = Number(displayBox.textContent) / 100;
+            break;
+        case '.':
+            (!displayBox.textContent.includes('.')) ? displayBox.textContent += '.': null;
+            break;
+        case '=':
+            evaluateExpression();
+            break;
     }
-    let num1 = Number.parseFloat(display.textContent);
-    if (expression.length < 1) {
-        clearSlate = true;
-    }
-    console.log(num1);
-    let operator = e.target.value;
-    console.log(operator);
-    return [num1, operator];
 }
 
-// Finish the calculation
-
-function calculate(expression) {
-    // Catch an error where the user tries to run a calculation without entering an operator
-    if (expression.length === 0) {
-        return;
+function evaluateExpression() {
+    expression.push(Number(displayBox.textContent));
+    switch(expression[1]) {
+        case '+':
+            displayBox.textContent = expression[0] + expression[2];
     }
-
-    let num1 = expression[0]
-    let operator = expression[1]
-    let num2 = Number.parseFloat(display.textContent)
-    let result;
-    console.log(num1, operator, num2)
-    if (operator === "+") {
-        result = (num1 + num2).toPrecision(10);
-    }
-    else if (operator === "-") {
-        result = (num1 - num2).toPrecision(10);
-    }
-    else if (operator === "*") {
-        result = (num1 * num2).toPrecision(10);
-    }
-    else if (operator === "/") {
-        result = (num1 / num2).toPrecision(10);
-    }
-
-    // Display the result with unecessary zeros chopped off.
-    display.textContent = parseFloat(result);
-
-    // Return an empty set of numbers and operators so pressing = again won't repeat calculation
-    return [];
 }
